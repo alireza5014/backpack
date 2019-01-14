afc7ef16ceb766b4d4ef2ca994545f67be05b466
alireza <persian402@gmail.com>
2019-01-14 15:31:17 +0330

    first commit

---
+++ app/Http/Controllers/Admin/ArticleCrudController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+
+// VALIDATION: change the requests to match your own file names if you need form validation
+use App\Http\Requests\ArticleRequest as StoreRequest;
+use App\Http\Requests\ArticleRequest as UpdateRequest;
+
+/**
+ * Class ArticleCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read CrudPanel $crud
+ */
+class ArticleCrudController extends CrudController
+{
+    public function setup()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | CrudPanel Basic Information
+        |--------------------------------------------------------------------------
+        */
+        $this->crud->setModel('App\Models\Article');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/article');
+        $this->crud->setEntityNameStrings('article', 'articles');
+
+        /*
+        |--------------------------------------------------------------------------
+        | CrudPanel Configuration
+        |--------------------------------------------------------------------------
+        */
+//        $this->crud->addColumn(['slug']);
+
+        // TODO: remove setFromDb() and manually define Fields and Columns
+//        $this->crud->setFromDb();
+         $this->crud->addField('title');
+         $this->crud->addField(['name'=>'description','type'=>'textarea']);
+
+
+         $this->crud->addColumn('id');
+         $this->crud->addColumn('title');
+         $this->crud->addColumn('description');
+         $this->crud->addColumn('created_at');
+
+
+
+
+        // add asterisk for fields that are required in ArticleRequest
+        $this->crud->setRequiredFields(StoreRequest::class, 'create');
+        $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        // your additional operations before save here
+        $request['user_id']=backpack_auth()->user()->id;
+        $request['slug']=$request->title;
+        $redirect_location = parent::storeCrud($request);
+        // your additional operations after save here
+        // use $this->data['entry'] or $this->crud->entry
+        return $redirect_location;
+    }
+
+    public function update(UpdateRequest $request)
+    {
+        // your additional operations before save here
+        $redirect_location = parent::updateCrud($request);
+        // your additional operations after save here
+        // use $this->data['entry'] or $this->crud->entry
+        return $redirect_location;
+    }
+}
